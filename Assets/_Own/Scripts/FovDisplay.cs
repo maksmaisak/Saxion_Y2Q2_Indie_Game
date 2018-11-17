9d465e68134c9b3f412fb485f1de335c94971db8
Maks <max.maisak@gmail.com>
2018-11-17 16:43:52 +0100

    Aiming cone visualization

---
+++ Assets/_Own/Scripts/FovDisplay.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct FovInfo
+{
+    public float maxAngle;
+    public float maxDistance;
+}
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class FovDisplay : MonoBehaviour
+{
+    [SerializeField] uint numBaseRays = 3;
+    [SerializeField] FovInfo _fov = new FovInfo {maxAngle = 30f, maxDistance = 30f};
+    [SerializeField] LayerMask scanLayerMask = Physics.DefaultRaycastLayers;
+    
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private MeshRenderer renderer;
+    private float baseAlpha;
+
+    public FovInfo fov
+    {
+        get { return _fov; }
+        set { _fov = value; }
+    }
+
+    public float fade
+    {
+        set
+        {
+            Color color = renderer.material.color;
+            color.a = value * baseAlpha;
+            renderer.material.color = color;
+        }
+    }
+   
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        baseAlpha = renderer.sharedMaterial.color.a;
+        
+        OnValidate();
+    }
+
+    void OnValidate()
+    {
+        if (vertices == null || vertices.Length != numBaseRays + 1)
+        {
+            mesh = new Mesh {name = "FovMesh"};
+            GetComponent<MeshFilter>().mesh = mesh;
+
+            vertices = new Vector3[numBaseRays + 1];
+            triangles = new int[(numBaseRays - 1) * 3];
+        }
+        
+        GenerateMesh();
+    }
+    
+    void LateUpdate()
+    {
+        GenerateMesh();
+    }
+
+    private void GenerateMesh()
+    {
+        Vector3 origin = transform.position;
+        Vector3 forward = transform.forward;
+        vertices[0] = Vector3.zero;
+        
+        Vector3 axis = transform.up;
+        var rotation  = Quaternion.AngleAxis(-fov.maxAngle * 0.5f, axis);
+        var increment = Quaternion.AngleAxis(fov.maxAngle / (numBaseRays - 1), axis);
+        for (int i = 0; i < numBaseRays; ++i, rotation *= increment)
+        {
+            var ray = new Ray(origin, rotation * forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, fov.maxDistance, scanLayerMask))
+            {
+                vertices[i + 1] = transform.InverseTransformPoint(hit.point);
+            }
+            else
+            {
+                vertices[i + 1] = transform.InverseTransformPoint(ray.GetPoint(fov.maxDistance));
+            }
+        }
+
+        for (int i = 0; i < numBaseRays - 1; ++i)
+        {
+            int triangleIndex = i * 3;
+            triangles[triangleIndex] = 0;
+            triangles[triangleIndex + 1] = i;
+            triangles[triangleIndex + 2] = i + 1;
+        }
+        
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
--- Assets/_Own/Scripts/PlayerShootingController.cs
+++ Assets/_Own/Scripts/PlayerShootingController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine.Utility;
 using UnityEngine;
 using UnityEngine.Assertions;
+using DG.Tweening;
+using DG.Tweening.Core.Easing;
 
 public class PlayerShootingController : MonoBehaviour
 {
@@ -10,8 +13,16 @@ public class PlayerShootingController : MonoBehaviour
     [SerializeField] Transform bulletSpawnLocation;
     [SerializeField] float reloadInterval = 1f;
     [SerializeField] float bulletSpeed = 20f;
+    [Space]
+    [SerializeField] FovInfo currentAimingFov = new FovInfo {maxAngle = 15, maxDistance = 30f};
+    [SerializeField] FovInfo noAimingFov = new FovInfo {maxAngle = 15f, maxDistance = 30f};
+    [SerializeField] FovDisplay fovDisplay;
+    [SerializeField] float aimingAngleDecreasePerSecond = 10f;
+    [SerializeField] float aimingAngleIncreasePerSecond = 40f;
+    [SerializeField] float maxSafeAimingTargetMovementPerSecond = 1f;
 
     private float timeWhenCanShoot;
+    private Vector3 previousAimingTargetPosition;
 
     void Start()
     {
@@ -19,23 +30,72 @@ public class PlayerShootingController : MonoBehaviour
     }
 
     void Update()
+    {        
+        UpdateFovDisplay();
+        Shoot();
+        
+        previousAimingTargetPosition = aimingTarget.position;
+    }
+    
+    void UpdateFovDisplay()
+    {
+        if (!fovDisplay) return;
+
+        // direction
+        fovDisplay.transform.forward = (aimingTarget.position - transform.position).normalized;
+
+        // fade
+        float timeTillCanShoot = TimeTillCanShoot();
+        if (timeTillCanShoot > 0f)
+        {
+            // The last two parameters are not need for this function.
+            fovDisplay.fade = EaseManager.ToEaseFunction(Ease.InExpo)(reloadInterval - timeTillCanShoot, reloadInterval, 0, 0);
+        }
+        else
+        {
+            fovDisplay.fade = 1f;
+        }
+
+        // angle
+        if (!CanShoot())
+        {
+            currentAimingFov.maxAngle = noAimingFov.maxAngle;
+        }
+        else
+        {
+            Vector3 deltaAimingTarget = aimingTarget.transform.position - previousAimingTargetPosition;
+            if (deltaAimingTarget.sqrMagnitude > maxSafeAimingTargetMovementPerSecond *
+                maxSafeAimingTargetMovementPerSecond * Time.deltaTime * Time.deltaTime)
+            {
+                currentAimingFov.maxAngle += aimingAngleIncreasePerSecond * Time.deltaTime;
+            }
+            else
+            {
+                currentAimingFov.maxAngle -= aimingAngleDecreasePerSecond * Time.deltaTime;
+                if (currentAimingFov.maxAngle < 0f) currentAimingFov.maxAngle = 0.01f;
+            }
+        }
+        currentAimingFov.maxAngle = Mathf.Clamp(currentAimingFov.maxAngle, 1f, noAimingFov.maxAngle);
+        fovDisplay.fov = currentAimingFov;
+    }
+
+    void Shoot()
     {
         if (!aimingTarget) return;
         if (!CanShoot()) return;
         if (!Input.GetMouseButton(0)) return;
 
-        var spawnLocation = bulletSpawnLocation ?? transform;
+        var spawnLocation = bulletSpawnLocation ? bulletSpawnLocation : transform;
         Vector3 position = spawnLocation.position;
-        Vector3 toTarget = (aimingTarget.position - position).normalized;
+        Vector3 toTarget = (aimingTarget.position - position).ProjectOntoPlane(Vector3.up).normalized;
         var bullet = Instantiate(bulletPrefab, position, Quaternion.LookRotation(toTarget));
 
         bullet.GetComponent<Rigidbody>().velocity = toTarget * bulletSpeed;
         
-        timeWhenCanShoot = Time.unscaledTime + reloadInterval;
+        timeWhenCanShoot = Time.time + reloadInterval;
     }
-
-    bool CanShoot()
-    {
-        return Time.unscaledTime >= timeWhenCanShoot;
-    }
+    
+    bool CanShoot() => Time.time >= timeWhenCanShoot;
+    float TimeTillCanShoot() => timeWhenCanShoot - Time.time;
+    
 }
