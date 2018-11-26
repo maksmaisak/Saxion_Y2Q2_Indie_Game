15064f51364ceec8695f2fdded01f4e1d263ccc3
Maks <max.maisak@gmail.com>
2018-11-26 23:49:39 +0100

    Enemy/Projectiles: prevent AutoDestroyParticleSystem from destroying early

--- Assets/_Own/Scripts/Utility/AutoDestroyParticleSystem.cs
+++ Assets/_Own/Scripts/Utility/AutoDestroyParticleSystem.cs
@@ -2,29 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /// Destroys the gameobject once all child particle systems and audio sources have finished playing.
 public class AutoDestroyParticleSystem : MonoBehaviour
 {
-    void Start()
+    private ParticleSystem[] particleSystems;
+    private AudioSource[] audioSources;
+    
+    IEnumerator Start()
     {
-        float duration = Mathf.Max(GetParticlesDuration(), GetAudioDuration());
-        if (!float.IsPositiveInfinity(duration))
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
+        audioSources = GetComponentsInChildren<AudioSource>();
+        
+        float minDuration = Mathf.Max(GetParticlesDuration(), GetAudioDuration());
+        if (!float.IsPositiveInfinity(minDuration))
         {
-            Destroy(gameObject, duration);
+            yield return new WaitForSeconds(minDuration);
         }
+
+        yield return new WaitUntil(() => 
+            !particleSystems.Any(ps => ps.IsAlive() && !audioSources.Any(a => a.isPlaying))
+        );
+        Destroy(gameObject, minDuration);
     }
 
     private float GetParticlesDuration()
     {
-        ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
         if (particleSystems.Length == 0) return 0f;
         return particleSystems.Max(ps => GetParticleSystemDuration(ps));
     }
 
     private float GetAudioDuration()
     {
-        AudioSource[] audioSources = GetComponentsInChildren<AudioSource>();
         if (audioSources.Length == 0) return 0f;
         return audioSources.Max(source => source.clip.length);
     }
@@ -34,7 +44,12 @@ public class AutoDestroyParticleSystem : MonoBehaviour
         float emissionDuration = particleSystem.main.duration;
 
         // Does not account for other MinMaxCurve modes, but should do for now.
-        float particleLifetime = particleSystem.main.startLifetime.constant;
+        ParticleSystem.MinMaxCurve startLifetime = particleSystem.main.startLifetime;
+        if (startLifetime.mode == ParticleSystemCurveMode.Constant)
+        {
+            Debug.LogWarning("AutoDestroyParticleSystem doesn't support non-constant startLifetime settings yet. The particle system might be destroyed earlier than needed.");
+        }
+        float particleLifetime = startLifetime.constant;
 
         return emissionDuration + particleLifetime;
     }
