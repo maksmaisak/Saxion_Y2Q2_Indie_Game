99ad8570bc4e8d4e2678fc556519dc743c6790ac
Maks <max.maisak@gmail.com>
2018-12-02 22:24:35 +0100

    Player/Shooting: made bullets apply force to killed targets

--- Assets/_Own/Scripts/Bullet.cs
+++ Assets/_Own/Scripts/Bullet.cs
@@ -6,20 +6,21 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] int damage = 1;
     [SerializeField] string ignoreTag = string.Empty;
+    [SerializeField] float hitImpulseMultiplier = 1f;
     [Space] 
     [SerializeField] UnityEvent OnHit;
             
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (string.IsNullOrWhiteSpace(ignoreTag) || !other.gameObject.CompareTag(ignoreTag))
+        if (string.IsNullOrWhiteSpace(ignoreTag) || !collision.gameObject.CompareTag(ignoreTag))
         {
-            var health = other.gameObject.GetComponentInChildren<Health>();
-            if (!health) health = other.gameObject.GetComponentInParent<Health>();
+            var health = collision.gameObject.GetComponentInChildren<Health>();
+            if (!health) health = collision.gameObject.GetComponentInParent<Health>();
             if (health)
             {
                 this.DoNextFrame(() =>
                 {
-                    health.DealDamage(damage);
+                    DealDamage(collision, health);
                     Destroy(gameObject);
                     OnHit.Invoke();
                 });
@@ -29,5 +30,18 @@ public class Bullet : MonoBehaviour
         
         OnHit.Invoke();
         Destroy(gameObject);
+    }
+
+    private void DealDamage(Collision collision, Health health)
+    {
+        bool wasAlive = health.isAlive;
+        health.DealDamage(damage);
+        bool didDie = wasAlive && health.isDead;
+        if (!didDie) return;
+        
+        var rb = collision.gameObject.GetComponentInParent<Rigidbody>();
+        if (rb == null) return;
+        
+        rb.AddForce(-collision.impulse * hitImpulseMultiplier, ForceMode.Impulse);
     }
 }
