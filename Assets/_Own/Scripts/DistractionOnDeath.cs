70e1bf3d13629ffc5701ae1b39ca3cb21b498df8
Maks <max.maisak@gmail.com>
2018-12-03 00:31:08 +0100

    Distractions/Editor: add custom editor for distraction hearing range

--- Assets/_Own/Scripts/DistractionOnDeath.cs
+++ Assets/_Own/Scripts/DistractionOnDeath.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -8,6 +9,42 @@ public class DistractionOnDeath : MonoBehaviour
     [SerializeField] float distractionPriority = 6f;
     [SerializeField] bool overrideEnemyHearingRadius = true;
     [SerializeField] float enemyHearingRadius = 15f;
+    
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(DistractionOnDeath))]
+    public class DistractionOnDeathEditor : Editor
+    {
+        private readonly Color colorSurfaces = new Color(0f, 0f, 1f, 0.02f);
+        private readonly Color colorHandles  = new Color(0f, 0f, 1f, 1f   );
+        
+        void OnSceneGUI()
+        {
+            var distractionOnDeath = (DistractionOnDeath)target;
+            if (!distractionOnDeath.overrideEnemyHearingRadius) return;
+            float hearingRadius = distractionOnDeath.enemyHearingRadius;
+
+            Transform transform = distractionOnDeath.transform;
+            Vector3 position = transform.position;
+        
+            Handles.color = colorSurfaces;
+            Handles.DrawSolidDisc(position, transform.up, hearingRadius);
+            
+            EditorGUI.BeginChangeCheck();
+            
+            Handles.color = colorHandles;
+            hearingRadius = Handles.RadiusHandle(
+                transform.rotation, 
+                position, 
+                hearingRadius
+            );
+            
+            if (!EditorGUI.EndChangeCheck()) return;
+            
+            Undo.RecordObject(distractionOnDeath, "Change distraction hearing radius.");
+            distractionOnDeath.enemyHearingRadius = hearingRadius;
+        }
+    }
+    #endif
 
     void Start()
     {
