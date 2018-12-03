7e03d1e2937c0619792fb69f4f8b56bc769ceaaf
Maks <max.maisak@gmail.com>
2018-12-03 18:06:44 +0100

    Menus: basic foundation for main menu and settings

---
+++ Assets/_Own/Scripts/UI/MenuScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MenuScreen : TransitionableScreen
+{
+	[SerializeField] FadeZoom fadeZoom;
+	
+	protected override void OnTransitionIn()
+	{
+		fadeZoom.FadeIn(canvasGroup, transform);
+		SelectFirstButton();
+	}
+
+	protected override void OnTransitionOut()
+	{
+		fadeZoom.FadeOut(canvasGroup, transform);
+	}
+}
