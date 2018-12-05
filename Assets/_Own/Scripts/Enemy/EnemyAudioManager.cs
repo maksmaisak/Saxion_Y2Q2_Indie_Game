7469c7568490ade3385bf1b04b983d1250cf029c
CocoDev <c.biti12@gmail.com>
2018-12-05 21:42:11 +0100

    Enemy/Sounds: Implement footstep sounds

---
+++ Assets/_Own/Scripts/Enemy/EnemyAudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Random = UnityEngine.Random;
+
+public class EnemyAudioManager : MonoBehaviour {
+
+	[SerializeField] AudioClip[] footStepAudioClips;
+	[SerializeField] AudioSource footStepAudioSource;
+	
+	void Start()
+	{
+		Assert.IsNotNull(footStepAudioSource);
+		Assert.IsTrue(footStepAudioClips.Length > 0);
+	}
+    
+	// Function called with animation key events
+	void PlayFootStepSound()
+	{
+		footStepAudioSource.clip = footStepAudioClips[Random.Range(0, footStepAudioClips.Length - 1)];
+		footStepAudioSource.Play();
+	}
+}
