0832206a27651fa7d8efeb2aa8315d3e4cc80eaa
Maks <max.maisak@gmail.com>
2018-12-06 06:44:56 +0100

    Made the intro only show up the first time

--- Assets/_Own/Scripts/Intro/Intro.cs
+++ Assets/_Own/Scripts/Intro/Intro.cs
@@ -16,13 +16,23 @@ public class Intro : MonoBehaviour
 	[SerializeField] float fadeoutDelay = 1f;
 	[SerializeField] float fadeoutTime = 1f;
 
+	private bool keepTimeScaleZero = true;
+
 	IEnumerator Start()
 	{
+		if (LevelManager.instance.didRestartLevel)
+		{
+			Destroy(gameObject);
+			yield break;
+		}
+		
 		TimeHelper.timeScale = 0f;
+		keepTimeScaleZero = true;
+		PauseScreen.canPause = false;
 		
 		string text = textMesh.text;
 		textMesh.text = String.Empty;
-		yield return new WaitForSeconds(initialDelay);
+		yield return new WaitForSecondsRealtime(initialDelay);
 
 		Assert.IsNotNull(textMesh);
 
@@ -31,8 +41,19 @@ public class Intro : MonoBehaviour
 		seq.Append(textMesh.DOText(text, timePerLetter * text.Length));
 		seq.AppendInterval(fadeoutDelay);
 		seq.Append(canvasGroup.DOFade(0f, fadeoutTime));
-
+		
 		yield return seq.WaitForCompletion();
+		
+		keepTimeScaleZero = false;
 		TimeHelper.timeScale = 1f;
+		PauseScreen.canPause = true;
+	}
+
+	private void Update()
+	{
+		if (keepTimeScaleZero)
+		{
+			TimeHelper.timeScale = 0f;
+		}
 	}
 }
