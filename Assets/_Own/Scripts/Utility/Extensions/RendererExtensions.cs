840564a513994e46242dd8c6e657308ca4abe55b
Maks <max.maisak@gmail.com>
2018-12-06 23:36:55 +0100

    Fixed highlighting of lamps and bullets. Fixed enemies getting stuck in navmesh. Fixed floor zfighting in tutorial

--- Assets/_Own/Scripts/Utility/Extensions/RendererExtensions.cs
+++ Assets/_Own/Scripts/Utility/Extensions/RendererExtensions.cs
@@ -4,11 +4,12 @@ using UnityEngine.Assertions;
 
 public static class RendererExtensions
 {
-    public static Rect GetViewportBounds(this Renderer renderer, Camera camera)
+    public static Vector3[] GetBoundsCorners(this Renderer renderer)
     {
         Vector3 min = renderer.bounds.min;
         Vector3 max = renderer.bounds.max;
-        Vector2[] viewportBorders = new []{
+        return new[]
+        {
             new Vector3(min.x, min.y, min.z),
             new Vector3(min.x, min.y, max.z),
             new Vector3(min.x, max.y, min.z),
@@ -17,7 +18,15 @@ public static class RendererExtensions
             new Vector3(max.x, min.y, max.z),
             new Vector3(max.x, max.y, min.z),
             new Vector3(max.x, max.y, max.z)
-        }.Select(b => (Vector2)camera.WorldToViewportPoint(b)).ToArray();
+        };
+    }
+    
+    public static Rect GetViewportBounds(this Renderer renderer, Camera camera)
+    {
+        Vector2[] viewportBorders = renderer
+            .GetBoundsCorners()
+            .Select(b => (Vector2)camera.WorldToViewportPoint(b))
+            .ToArray();
 		
         Vector2 minViewportPosition = new Vector2(viewportBorders.Min(b => b.x), viewportBorders.Min(b => b.y));
         Vector2 maxViewportPosition = new Vector2(viewportBorders.Max(b => b.x), viewportBorders.Max(b => b.y));
